280a04c16ff03306216751d36f0e483467098146
kam <148465412+raspy-windbird@users.noreply.github.com>
2025-11-06 01:46:22 +0000

    href[i]からiに変更

--- assets/Link.js
+++ assets/Link.js
@@ -6,8 +6,8 @@ export class Link {
         console.log("helf: "+href);
         const newhref = [];
         for (let i of href) {
-            newhref.push("https://"+href[i]);
-            console.log("newhref add: "+i+" "+href[i])
+            newhref.push("https://"+i);
+            console.log("newhref add: "+i);
         }
         console.log("newhref: "+newhref);//test
         this.href = newhref;
@@ -15,7 +15,7 @@ export class Link {
     get open () {
         const links = this.href;    
         for (let i in links) {
-            window.open(links[i],"_blank");
+            window.open(i,"_blank");
         }
     }
 }
