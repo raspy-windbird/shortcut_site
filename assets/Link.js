d361cc8b030637e33d03aad2520366aeac723162
kam <148465412+raspy-windbird@users.noreply.github.com>
2025-11-06 01:42:45 +0000

    test用console.log

--- assets/Link.js
+++ assets/Link.js
@@ -3,15 +3,17 @@
  */
 export class Link {
     constructor(...href){
-        console.log("Link: "+href);
+        console.log("helf: "+href);
         const newhref = [];
         for (let i of href) {
-            newhref = newhref.push("https://"+href[i]);
+            newhref.push("https://"+href[i]);
+            console.log("newhref add: "+i+" "+href[i])
         }
         console.log("newhref: "+newhref);//test
         this.href = newhref;
     }
-    open (links = this.href) {
+    get open () {
+        const links = this.href;    
         for (let i in links) {
             window.open(links[i],"_blank");
         }
